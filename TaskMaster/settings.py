adbb221bb0625edef78cddbed391782a14768f55
Your Name <your.email@example.com>
2024-02-16 18:08:15 +0600

    updated

--- TaskMaster/settings.py
+++ TaskMaster/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Your secret key
-SECRET_KEY = env("SECRET_KEY")
+# SECRET_KEY = env("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
